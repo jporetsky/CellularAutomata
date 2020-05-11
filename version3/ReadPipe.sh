5e29eee57e22140bb9e6dc8f1b687841472451fc
Jake <jporetsky14@gmail.com>
2020-05-11 17:44:15 -0400

    adding temp file

---
+++ version3/ReadPipe.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ ! -p pipe ]]; then
+    echo "writer not running"
+    exit 1
+fi
+
+
+while true
+do
+    if read line <pipe; then
+        if [[ $line == '27' ]] 
+	then
+            break
+        fi
+        echo "$line"
+    fi
+done
+
+echo "Reader exiting"
